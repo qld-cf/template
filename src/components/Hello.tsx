38ee3f208f7b7b3a247f7bb4ea0a9c75e3887a7f
qld-cf <feng.cf@dtwave-inc.com>
2020-01-19 18:31:21 +0800

    子组件调用

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Hello1 from '@cps/component1/Hello1';
 class Hello extends Component {
   constructor (props: any) {
     super(props);
@@ -11,6 +12,7 @@ class Hello extends Component {
     return (
       <div className='hello'>
         <h2>Hello React</h2>
+        <Hello1 />
       </div>
     );
   }
--- src/components/component1/Hello1.tsx
+++ src/components/component1/Hello1.tsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-class Hello extends Component {
+interface IProps {
+}
+class Hello1 extends Component<IProps, any> {
   constructor (props: any) {
     super(props);
     this.state = {
-
+      helloState: 'hello world'
     };
+  }
+
+  shouldComponentUpdate (nextProps: IProps, nextState: any): boolean {
+    console.log('子组件数据2state或者redux store更新时，父组件也会重绘，尽量使用该方法提升性能');
+    // if(nextProps.helloState === this.props.number){
+    //   return true
+    // }
+    // return false
+    return false;
+  }
+
+  // react 16新增，17以后会向下兼容
+  // 接收到新的属性想去修改我们state
+  static getDerivedStateFromProps (nextProps, prevState): any {
+    if (nextProps.currentState !== prevState.helloState) {
+      return {
+        helloState:
+          nextProps.currentState > prevState.helloState,
+      };
+    }
+    return null;
   }
 
   render () {
@@ -15,4 +38,4 @@ class Hello extends Component {
     );
   }
 }
-export default Hello;
+export default Hello1;
